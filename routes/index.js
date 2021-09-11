8938a6305eb010f7fb6b317295e6a5ae03b2a387
shinoyuma <shino0812jjj@gmail.com>
2021-09-11 17:13:25 +0900

    ランダムで漢字を返すAPI作成

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,68 @@
 var express = require('express');
 var router = express.Router();
 
+// http://localhost:3001
+
+// GET: /todos
+// - DBに保存されているtodoの一覧を返す
+
+// POST: /todo
+// - DBにtodoを1個保存する
+
+// GET: /usesr/1
+// - user_idが1のユーザ情報を返す
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// Graphqlレスポンスっぽいの
-router.post('/graphql', (req, res) => {
-  if (!req.body.text)
-    return res.status(400).json({
-      msg: 'request bodyにtextを含めてください',
+router.get('/sample', (req, res) => {
+  // DBからidがreq.query.idの値をSELECTする
+
+  res.json({
+    id: req.query.id,
+    name: 'kmtym1998',
+    now: new Date(),
+  });
+});
+
+router.post('/sample', (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+
+  res.json({
+    id,
+    name,
+    now: new Date(),
+  });
+});
+
+router.get('/face', (req, res) => {
+  const name = req.query.name;
+
+  if (!name) {
+    res.status(400).json({
+      message: 'nameを入力してください',
     });
-  const privileged = req.body.text === 'privileged';
-  const msg = privileged ? 'user is privileged' : 'user is not privileged';
-  console.log({ msg });
-  return res.json({
-    data: {
-      me: {
-        store: {
-          tenant: {
-            id: 'user-id',
-            text: req.body.text,
-            privileged,
-          },
-        },
-      },
-    },
+  }
+
+  const allChars = ['H', '知', '熱', '金', '木', '水'];
+  const allCharsLength = allChars.length;
+  let characters = [];
+
+  for (let i = 0; i < 4; i++) {
+    const random = Math.floor(Math.random() * allCharsLength);
+    const selectedChar = allChars[random];
+    characters.push(selectedChar);
+  }
+
+  const count = Math.floor(Math.random() * 1000);
+
+  res.json({
+    characters: characters,
+    name: name,
+    count: count,
   });
 });
 
